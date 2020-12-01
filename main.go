f793f4ade87a763a5137879b044b2d73e2a34d42
Nixon Kosgei <nkosgey6@gmail.com>
2020-12-01 23:03:13 +0300

    added simple get request

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	response, err := http.Get("https://httpbin.org/get")
+	exitOnErr(err, "http Get request failed")
+
+	defer response.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	exitOnErr(err, "Failed to read response body")
+
+	bodyString := string(bodyBytes)
+
+	fmt.Println(bodyString)
+	fmt.Printf("StatusCode: %s\n", response.Status)
+	fmt.Printf("Headers: %s\n", response.Header)
+
+	fmt.Printf("Header: Content Type - %s", response.Header["Content-Type"])
+}
+
+func exitOnErr(err error, msg string) {
+	if err != nil {
+		log.Fatal(msg, err.Error())
+	}
 }
